fea171a1ec2be969ca084f9b5bc8e34cd4dbb40f
Daniel Tulpa <dtulpa16@yahoo.com>
2024-01-26 10:06:53 -0700

    Rendered Product Cards

--- src/Components/ProductCard.tsx
+++ src/Components/ProductCard.tsx
@@ -8,10 +8,11 @@ export default function ProductCard({ product }: ProductCardProps) {
   return (
     <div className="card w-96 bg-primary text-primary-content">
       <div className="card-body">
-        <h2 className="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <h2 className="card-title">{product.name}</h2>
+        <p>{product.description}</p>
+        <p className=" text-white font-bold text-xl">${product.price}</p>
         <div className="card-actions justify-end">
-          <button className="btn">Buy Now</button>
+          <button className="btn">Details</button>
         </div>
       </div>
     </div>
--- src/Components/ProductList.tsx
+++ src/Components/ProductList.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import { useLoaderData } from 'react-router-dom'
-import { Product } from '@/types'
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { Product } from "@/types";
+import ProductCard from "./ProductCard";
 export default function ProductList() {
-    const data = useLoaderData()
-    const products = data as Product[]
-    console.log("Products",products)
+  const data = useLoaderData();
+  const products = data as Product[];
+  console.log("Products", products);
   return (
-    <div>ProductList</div>
-  )
+    <div
+      className="w-full bg-cover bg-center min-h-screen flex items-center justify-center"
+      style={{
+        backgroundImage:
+          "url(https://images.unsplash.com/photo-1607082348824-0a96f2a4b9da?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+      }}
+    >
+      <div className="w-full max-w-[1292px] mx-auto py-12 flex flex-wrap gap-8 justify-center">
+        {products
+          ? products.map((el) => <ProductCard key={el._id} product={el} />)
+          : null}
+      </div>
+    </div>
+  );
 }
