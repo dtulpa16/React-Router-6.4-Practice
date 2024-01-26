ed0f275b609a3c1273b49f14403648d1f4e37c46
Daniel Tulpa <dtulpa16@yahoo.com>
2024-01-26 08:49:38 -0700

    Added Error Boudary

---
+++ src/Components/ErrorPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ErrorPage() {
+  return (
+    <div className="error-page-container">
+      <div className="error-content">
+        <h1>Oops!</h1>
+        <p>Something went wrong. We're working on it!</p>
+      </div>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import axios from 'axios';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./Components/ErrorPage"
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    loader: async () => {
+      let response = await axios.get(`${process.env.REACT_APP_BASE_URL}`)
+    },
+    errorElement:<ErrorPage/>
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
